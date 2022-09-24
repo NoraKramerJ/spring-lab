1345f84ec4848888ffc5de132f574fa90dd1a240
Nmerkel <noramerkel0@gmail.com>
2022-09-24 15:05:05 -0400

    Learning how to use @Pathvariable annotation

--- lab-04-springmvc/src/main/java/com/cydeo/controller/ProductController.java
+++ lab-04-springmvc/src/main/java/com/cydeo/controller/ProductController.java
@@ -1,21 +1,35 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.lab04springmvc.model.Product;
+import com.cydeo.lab04springmvc.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/search-product/{productName}")
-    public String searchProduct(@PathVariable(value="productName")  String productName, Model model){
+    public String searchProduct(@PathVariable(value="productName")
+           String productName, Model model){
 
 // @PathVariable annotation can be used to handle template variables in the request URI mapping
 // , and set them as method parameters.
 
 
+model.addAttribute("productList",
+        productService.searchProduct(productName) );
 
 
 
