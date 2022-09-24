eedc6c126e631d64f4cb3fe2721cf764177b2879
Nmerkel <noramerkel0@gmail.com>
2022-09-24 15:26:34 -0400

    Created stream to collect the cartList items to add to my cart list controller class

--- lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/service/impl/CartServiceImpl.java
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/service/impl/CartServiceImpl.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.cydeo.lab04springmvc.service.impl.ProductServiceImpl.PRODUCT_LIST;
 
@@ -22,7 +23,9 @@ public class CartServiceImpl implements CartService {
     @Override
     public List<CartItem> retrieveCartDetail(UUID cartId) {
         // todo implement method using stream
-        return new ArrayList<>();
+        return CART_LIST.stream().filter(cart->cart.getId().equals(cartId.toString()))
+                .map(p->p.getCartItemList())
+                .findAny().orElseThrow();
     }
 
     @Override
